d11de09948a0a7fb1ab2a7d07c8e4f940c909373
Mosin Inamdar <mosininamdar18@gmail.com>
2024-06-27 19:19:49 +0530

    Error solved

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,26 +6,29 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
-    path(route='register', view=views.register, name='register'),
+    # path for registration
+    path('register/', views.register, name='register'),
 
     # path for login
-    path(route='login', view=views.login_user, name='login'),
+    path('login/', views.login_user, name='login'),
 
     # path for logout 
-    path(route='logout', view=views.logout_request, name='logout'),
+    path('logout/', views.logout_request, name='logout'),
 
     # path for cars
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path('get_cars/', views.get_cars, name='getcars'),
 
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-        path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    # path for dealers
+    path('get_dealers/', views.get_dealerships, name='get_dealers'),
+    path('get_dealers/<str:state>/', views.get_dealerships, name='get_dealers_by_state'),
+
+    # path for dealer details
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
 
     # path for dealer reviews view
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path('reviews/dealer/<int:dealer_id>/', views.get_dealer_reviews, name='dealer_reviews'),
 
     # path for add a review view
-    path(route='add_review', view=views.add_review, name='add_review'),
+    path('add_review/', views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
