738d75576d497c44684c09ca33b296c6a7f90001
Mosin Inamdar <mosininamdar18@gmail.com>
2024-06-27 21:22:15 +0530

    done

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -117,8 +117,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NumericPasswordValidator')},
 ]
 
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 LANGUAGE_CODE = 'en-us'
