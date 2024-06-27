03a14406b3b259d1f470c6047daaf184dab114c2
Mosin Inamdar <mosininamdar18@gmail.com>
2024-06-27 19:49:18 +0530

    Error solving

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -114,10 +114,11 @@ def add_review(request):
     if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
         except Exception as e:
-            return JsonResponse({"status": 401, "message": f"Error in posting review: {e}"})
+            return JsonResponse({"status": 401,
+"message": f"Error in posting review: {e}"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
 
@@ -130,6 +131,7 @@ def get_cars(request):
         initiate()
 
     car_models = CarModel.objects.select_related('car_make')
-    cars = [{"CarModel": car_model.name, "CarMake": car_model.car_make.name} for car_model in car_models]
+    cars = [{"CarModel": car_model.name, "CarMake": car_model.car_make.name}
+for car_model in car_models]
 
     return JsonResponse({"CarModels": cars})
