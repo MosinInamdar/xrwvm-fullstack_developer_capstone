fccd344b4f7a9d9660ac314c735311f1ba91c19d
Mosin Inamdar <mosininamdar18@gmail.com>
2024-06-28 16:18:30 +0530

    Removed /

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,37 +7,37 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # path for registration
-    path('register/', views.register, name='register'),
+    path('register', views.register, name='register'),
 
     # path for login
-    path('login/', views.login_user, name='login'),
+    path('login', views.login_user, name='login'),
 
     # path for logout
-    path('logout/', views.logout_request, name='logout'),
+    path('logout', views.logout_request, name='logout'),
 
     # path for cars
-    path('get_cars/', views.get_cars, name='getcars'),
+    path('get_cars', views.get_cars, name='getcars'),
 
     # path for dealers
-    path('get_dealers/', views.get_dealerships, name='get_dealers'),
+    path('get_dealers', views.get_dealerships, name='get_dealers'),
     path(
-        'get_dealers/<str:state>/', views.get_dealerships,
+        'get_dealers/<str:state>', views.get_dealerships,
         name='get_dealers_by_state'
         ),
 
     # path for dealer details
     path(
-        'dealer/<int:dealer_id>/', views.get_dealer_details,
+        'dealer/<int:dealer_id>', views.get_dealer_details,
         name='dealer_details'
         ),
 
     # path for dealer reviews view
     path(
-        'reviews/dealer/<int:dealer_id>/', views.get_dealer_reviews,
+        'reviews/dealer/<int:dealer_id>', views.get_dealer_reviews,
         name='dealer_reviews'
         ),
 
     # path for add a review view
-    path('add_review/', views.add_review, name='add_review'),
+    path('add_review', views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -35,8 +35,6 @@ ALLOWED_HOSTS = [
      'prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'),
 ]
 
-APPEND_SLASH = False
-
 CSRF_TRUSTED_ORIGINS = [
     ('https://mosininamdar-8000.theianext-1-labs-prod-'
      'misc-tools-us-east-0.proxy.cognitiveclass.ai'),
